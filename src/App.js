1dd55711a96fb73d6449feb8212fe78363456b5c
Joshua Kelly <hewhodevs@outlook.com>
2020-10-21 00:12:57 +1030

    started DropRight component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./styles.css";
 import styled from "styled-components";
 import NavBar from "./Components/NavBar";
+import DropRight from "./Components/DropRight";
 
 export default function App() {
   return (
     <StyledApp className="App">
-      <NavBar />
+      <div className="columnContainer">
+        <DropRight title="Label 1" children={["Greetings","fellow traveller","stay a while, and listen"]} />
+        <DropRight title="Label 2" children={["You shall not pass!", <span><img src="https://media4.giphy.com/media/ONostKY8aj8bK/source.gif"></img></span>,"Gandalf!"]} />
+      </div>
     </StyledApp>
   );
 }
@@ -14,4 +18,6 @@ export default function App() {
 const StyledApp = styled.div`
   height: 100vh;
   box-sizing: border-box;
+  background-color: #e8e8e8;
+  margin: 40px;
 `;
--- src/Components/DropRight.jsx
+++ src/Components/DropRight.jsx
@@ -1,14 +1,61 @@
 import React from "react";
 import styled from "styled-components";
 
-const DropRightContainer = styled.div``;
+// --------------------------
+// Component Styles
+// --------------------------
+
+const DropRightContainer = styled.div`
+  display: flex;
+  height: 38px;  /* why does this work?*/
+
+  .parentContainer {
+    cursor: pointer;
+    &:hover + .children {
+      display: flex;
+    }
+  }
+  .parent {
+    background: lightblue;
+    padding: 10px;
+    &:hover {
+      background-color: #49dffc;
+    }
+  }
+  .children {
+    display: none;
+    flex-direction: column;
+    &:hover {
+      display: flex;
+    }
+  }
+  .child {
+    cursor: pointer;
+    padding: 10px;
+    background-color: lightblue;
+    border: 1px solid darkblue;
+    &:hover {
+      background-color: #49dffc;
+    }
+  }
+`;
+
+// --------------------------
+// React Components
+// --------------------------
 
 export default class DropRight extends React.Component {
   render() {
-    return (
+    return(
       <DropRightContainer>
-        <div className="parent"></div>
-        <div className="children"></div>
+        <div className="parentContainer">
+          <div className="parent">{this.props.title}</div>
+        </div>
+        <div className="children">
+          {this.props.children.map((child, index) => {
+            return <span className="child" key={index} >{child}</span>
+          })}
+        </div>
       </DropRightContainer>
     );
   }
